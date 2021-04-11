c4c275d164b1e281335e477950c7c1343b4fb8cb
sigit hadi prawira <sigithadiprawira@gmail.com>
2021-04-12 00:01:16 +0800

    update responsive

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      initialRoute: Routes.ADMIN,
+      initialRoute: Routes.GUEST,
       getPages: Pages.getPage,
     );
   }
--- lib/pages/pageGuest.dart
+++ lib/pages/pageGuest.dart
@@ -6,90 +6,92 @@ class PageGuest extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final controller = Get.put(GuestController());
-
+    final size = Get.size;
     return Scaffold(
       body: Container(
         height: Get.height,
         width: Get.width,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Image.asset(
-              "assets/logo.png",
-              height: Get.height - 500,
-            ),
-            SizedBox(height: 10),
-            Text("Selamat Datang", style: Get.textTheme.headline3),
-            Text("Silakan masukkan data diri", style: Get.textTheme.headline4),
-            SizedBox(height: 10),
-            Container(
-              padding: EdgeInsets.symmetric(horizontal: 200),
-              child: Form(
-                key: controller.formKey,
-                child: Column(
-                  children: [
-                    TextFormField(
-                      decoration: InputDecoration(
-                        labelText: "Nama",
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Image.asset(
+                "assets/logo.png",
+                height: Get.height - 500,
+              ),
+              SizedBox(height: 10),
+              Text("Selamat Datang", style: Get.textTheme.headline3),
+              Text("Silakan masukkan data diri", style: Get.textTheme.headline4),
+              SizedBox(height: 10),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 10),
+                child: Form(
+                  key: controller.formKey,
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        decoration: InputDecoration(
+                          labelText: "Nama",
+                        ),
+                        controller: controller.nama,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return "Nama tidak boleh kosong";
+                          }
+                          return null;
+                        },
                       ),
-                      controller: controller.nama,
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return "Nama tidak boleh kosong";
-                        }
-                        return null;
-                      },
-                    ),
-                    TextFormField(
-                      decoration: InputDecoration(
-                        labelText: "No. Hp",
+                      TextFormField(
+                        decoration: InputDecoration(
+                          labelText: "No. Hp",
+                        ),
+                        controller: controller.phone,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return "No. Hp tidak boleh kosong";
+                          }
+                          return null;
+                        },
                       ),
-                      controller: controller.phone,
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return "No. Hp tidak boleh kosong";
-                        }
-                        return null;
-                      },
-                    ),
-                    TextFormField(
-                      decoration: InputDecoration(
-                        labelText: "Email",
-                      ),
-                      controller: controller.email,
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return "Email tidak boleh kosong";
-                        }
+                      TextFormField(
+                        decoration: InputDecoration(
+                          labelText: "Email",
+                        ),
+                        controller: controller.email,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return "Email tidak boleh kosong";
+                          }
 
-                        if (!GetUtils.isEmail(value)) {
-                          return "Masukkan email yang benar";
-                        }
+                          if (!GetUtils.isEmail(value)) {
+                            return "Masukkan email yang benar";
+                          }
 
-                        return null;
-                      },
-                    ),
-                    TextFormField(
-                      decoration: InputDecoration(
-                        labelText: "Keperluan",
+                          return null;
+                        },
                       ),
-                      controller: controller.keperluan,
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return "Email tidak boleh kosong";
-                        }
+                      TextFormField(
+                        decoration: InputDecoration(
+                          labelText: "Keperluan",
+                        ),
+                        controller: controller.keperluan,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return "Email tidak boleh kosong";
+                          }
 
-                        return null;
-                      },
-                    ),
-                  ],
+                          return null;
+                        },
+                      ),
+                    ],
+                  ),
                 ),
               ),
-            ),
-            SizedBox(height: 10),
-            ElevatedButton.icon(onPressed: controller.onRegister, icon: Icon(Icons.login), label: Text("Register"))
-          ],
+              SizedBox(height: 10),
+              ElevatedButton.icon(onPressed: controller.onRegister, icon: Icon(Icons.login), label: Text("Register"))
+            ],
+          ),
         ),
       ),
     );
